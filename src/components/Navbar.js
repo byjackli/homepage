e57e9e03ea8557d1a60e82dd5cf1b9652839f603
Jack Li <jack7598@gmail.com>
2020-05-21 22:58:56 -0400

    add navbar
    - add navbar
    - reafactored searchbar
    
    #10 #19

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+import Menubar from '../components/Menubar';
+import Searchbar from '../components/Searchbar';
 
 function Navbar() {
 
-    return null;
+    // function toggleMenu() {
+    //     return <Menubar />;
+    // }
+
+    return (
+        <nav>
+            <div className="left">
+                <Link to="#"><i class="fas fa-bars"></i></Link>
+                <Link to="/">byjackli</Link>
+                <Searchbar />
+            </div>
+            <div>
+                <NavLink to="/Resume">Resume</NavLink>
+                <NavLink to="/About">About</NavLink>
+                <NavLink to="/Projects">Projects</NavLink>
+                <NavLink to="/SignIn">Sign In</NavLink>
+                <button className="button-pill-fill">Create Account</button>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 
 function Searchbar() {
-    function renderSearch(){
-        // displays search suggestions
-    }
+    // function renderSearch(){
+    //     // displays search suggestions
+    // }
 
-    return <input className="fa-search" type="text" placeholder="&#xF002; Search byjackli" />;
+    return <input className="searchbar fa-search" type="text" placeholder="&#xF002; Search byjackli" />;
 }
 
 export default Searchbar;
