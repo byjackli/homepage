ccdcd43e1de1a6dcf5749cb11ece647ee4c0dab2
Jack Li <jack7598@gmail.com>
2020-05-21 20:50:17 -0400

    add search bar
    - add search bar
    - add Font Awesome 5 Free to font family
    - removed redundant index.css file
    
    #17

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
-function Searchbar() {
 
-    return null;
+function Searchbar() {
+    function renderSearch(){
+        // displays search suggestions
+    }
+
+    return <input className="fa-search" type="text" placeholder="&#xF002; Search byjackli" />;
 }
 
 export default Searchbar;
--- src/pages/Styleguide.js
+++ src/pages/Styleguide.js
@@ -12,7 +12,7 @@ function Styleguide() {
     return (
         <main className="styleguide">
             <h1>Styleguide</h1>
-            <section>
+            <section className="navigation">
                 <h2>Navigation</h2>
                 <Navbar />
                 <Minibar />
@@ -20,19 +20,19 @@ function Styleguide() {
                 <Searchbar />
                 <Footer />
             </section>
-            <section>
+            <section className="typography">
                 <h2>Typography</h2>
             </section>
             <section>
                 <h2>Color Palette</h2>
             </section>
-            <section>
+            <section className="buttons-and-links">
                 <h2>Buttons and Links</h2>
                 <button className="button-box-fill">box button</button>
                 <button className="button-pill-fill">pill button</button>
                 <Link className="intext" to="#"><i className="fas fa-link"></i> <span>in-text link</span></Link>
             </section>
-            <section>
+            <section className="cards-and-slots">
                 <h2>Cards and Slots</h2>
                 <Card class="card" header="Card with Text" elements={
                     <div>
@@ -40,12 +40,12 @@ function Styleguide() {
                     </div>} />
                 <Card class="card" header="Card with Slots" elements={
                     <div className="card-cap">
-                        <Slot month="September" date="22" header="background image" image="https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/e35/70515020_421309365191643_6585258917258799635_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=103&_nc_ohc=Zolkr2asqEEAX-A_w3N&oh=26584734bf49b0404bc4115dd3a9d89e&oe=5EF10579"/>
+                        <Slot month="September" date="22" header="background image" image="https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/e35/70515020_421309365191643_6585258917258799635_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=103&_nc_ohc=Zolkr2asqEEAX-A_w3N&oh=26584734bf49b0404bc4115dd3a9d89e&oe=5EF10579" />
                         <Slot month="May" date="4" header="no background image" />
-                        <Slot month="July" date="11" header="background image" image="https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/e35/62054280_837856996600363_8628047517587627838_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=107&_nc_ohc=uOIFhXKuPo4AX-o6lwg&oh=c0577da8f21509d64e81733abdf82b0c&oe=5EF183EB" tags={["boat","car","train"]}/>
+                        <Slot month="July" date="11" header="background image" image="https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/e35/62054280_837856996600363_8628047517587627838_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=107&_nc_ohc=uOIFhXKuPo4AX-o6lwg&oh=c0577da8f21509d64e81733abdf82b0c&oe=5EF183EB" tags={["boat", "car", "train"]} />
                     </div>} />
                 <Slot month="September" date="22" header="no background image" />
-                <Slot month="May" date="22" header="background image" image="https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/e35/62054280_837856996600363_8628047517587627838_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=107&_nc_ohc=uOIFhXKuPo4AX-o6lwg&oh=c0577da8f21509d64e81733abdf82b0c&oe=5EF183EB" tags={["boat","car","train"]}/>
+                <Slot month="May" date="22" header="background image" image="https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/e35/62054280_837856996600363_8628047517587627838_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=107&_nc_ohc=uOIFhXKuPo4AX-o6lwg&oh=c0577da8f21509d64e81733abdf82b0c&oe=5EF183EB" tags={["boat", "car", "train"]} />
             </section>
         </main>
     )
