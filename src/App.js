89bf3af21d3f8f5f4ceb41bf66c94fc72b280385
byjackli <jack7598@gmail.com>
2020-05-24 12:57:42 -0400

    add resume page
    - hard coded resume page
    
    #1 #23

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './styles/App.css';
 
-import Styleguide from './pages/Styleguide'
+import './styles/App.css';
+import { Navbar, Footer } from './components/Navigation';
+import Resume from './pages/Resume';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Styleguide from './pages/Styleguide';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Navbar />
         <Switch>
+          <Route path={["/", "/resume"]} exact component={Resume} />
+          <Route path="/about" exact component={About} />
+          <Route path="/projects" exact component={Projects} />
+          <Route path="/Signin" exact component={Signin} />
+          <Route path="/Signup" exact component={Signup} />
           <Route path="/styleguide" exact component={Styleguide} />
         </Switch>
+        <Footer />
       </div>
     </Router>
   );
---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+
+    return (
+        <main className="about">
+
+        </main>
+    );
+}
+
+export default About;
